c5b5404b1296fe0a4f03b6a34b580aea738d31d5
Xiaoling Tong <tonxiaol@sheridancollege.ca>
2023-05-26 14:40:33 -0400

    add a Branch

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,4 +1,7 @@
 package exercise1;
+import static exercise1.Card.SUITS;
+import static java.lang.Math.random;
+import java.util.Scanner;
 
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
@@ -6,7 +9,7 @@ package exercise1;
  * To be used as starting code in Exercise
  *
  * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Xiaoling Tong May 26th, 2023
  */
 public class CardTrick {
     
@@ -16,9 +19,8 @@ public class CardTrick {
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            card.setValue(random.nextInt(14));         
+            card.setSuit(Card.SUITS[random.nextInt(4)]);
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
         }
@@ -32,13 +34,26 @@ public class CardTrick {
         // Then loop through the cards in the array to see if there's a match.
         
         // If the guess is successful, invoke the printInfo() method below.
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Guess the card number (enter a number from 1 to 13: ");
+        int guessedCardNumber = input.nextInt();  // Read user input
+        
+        System.out.println("Guess the card suit (enter a number from 1 to 4: ");
+        int guessedCardSuit = input.nextInt();  // Read user input
+        
+        for (Card hand1 : hand) {
+            if (hand1.getValue() == guessedCardNumber && hand1.getSuit().equals(SUITS[guessedCardSuit-1]) ) {
+                printInfo();
+            }
+        }
         
     }
 
     /**
      * A simple method to print out personal information. Follow the instructions to 
      * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
+     * @author Xiaoling Tong May 26th, 2023
      */
     private static void printInfo() {
     
@@ -64,4 +79,6 @@ public class CardTrick {
     
     }
 
+
 }
+
